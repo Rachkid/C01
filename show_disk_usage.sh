7bedb7be3002cb8e17968ccdab34a5832897d52b
Rachkid <abouzeidirachid97@gmail.com>
2025-05-18 03:35:11 +0100

    Create show_disk_usage.sh

---
+++ show_disk_usage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#ce code affiche l'utilisation fait sur le disk d'une façon lisible 
+#df: la commande df affiche l'utilisation de l'espace du disk
+#-h: cette commande quant elle permet de rendre la sortie d'une façon lisible 
+
+echo "utilisation du disk:"
+
+df -h
