a2be2221670a94e02e894630f8d757c971cc3d63
Rachkid <abouzeidirachid97@gmail.com>
2025-05-18 03:39:08 +0100

    Update simple_calculatrice.sh

--- simple_calculatrice.sh
+++ simple_calculatrice.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+# ce code permet d'effectuer des opérations mathématiques
+
+a=$1
+b=$2
+op=$3
+
+read a b op
+
+if [ "$op" = "+" ]; then
+    resultat=$((a + b))
+elif [ "$op" = "-" ]; then
+    resultat=$((a - b))
+elif [ "$op" = "*" ]; then
+    resultat=$((a * b))
+elif [ "$op" = "/" ]; then
+    resultat=$((a / b))
+fi
+
+echo "le resultat est : $resultat"
+
+exit 0
